fe40f255a27b9e669c65660f81a30a9518388036
Juhani Kauppi <jkauppi@student.hive.fi>
2023-07-21 19:37:27 +0300

    EX04 ready for test.

--- ex04/ft_calculator.py
+++ ex04/ft_calculator.py
@@ -1,22 +1,23 @@
 class calculator:
-    '''
-Vector calculator
-    '''
+    '''Vector calculator'''
     def  __init__(self) -> None:
         pass
 
     @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
-        mul_list:list = list(map(lambda x, y: x * y, V1, V2))
+        '''Dot product method'''
+        mul_list:list = list(map(lambda x, y: float(x * y), V1, V2))
         total:float = sum(mul_list)
         print('Dot product is: {}'.format(str(total)))
 
     @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
-        add_list:list = list(map(lambda x, y: x + y, V1, V2))
+        '''Addition method'''
+        add_list:list = list(map(lambda x, y: float(x + y), V1, V2))
         print('Add Vector is : {}'.format(str(add_list)))
 
     @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
-        add_list:list = list(map(lambda x, y: x * y, V1, V2))
+        '''Subtraction method'''
+        add_list:list = list(map(lambda x, y: float(x - y), V1, V2))
         print('Sous Vector is: {}'.format(str(add_list)))
--- ex04/tester.py
+++ ex04/tester.py
@@ -1,4 +1,5 @@
 from ft_calculator import calculator
+
 a = [5, 10, 2]
 b = [2, 4, 3]
 calculator.dotproduct(a,b)
@@ -7,4 +8,7 @@ calculator.sous_vec(a,b)
 
 calculator_obj:calculator = calculator()
 
-calculator_obj.dotproduct(a, b)
+print(calculator_obj.__doc__)
+print(calculator_obj.dotproduct.__doc__)
+print(calculator_obj.add_vec.__doc__)
+print(calculator_obj.sous_vec.__doc__)
